dc550a86794c0659bc8306c8160681e319505608
unknown <3275441767@qq.com>
2021-08-22 21:31:33 +0800

    fifth homework

--- Springboot-02-new/src/main/java/com/lan/controllor/UserContollor.java
+++ Springboot-02-new/src/main/java/com/lan/controllor/UserContollor.java
@@ -1,5 +1,6 @@
 package com.lan.controllor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lan.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,10 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserContollor {
     @Autowired
     UserMapper userMapper;
-    @RequestMapping("/find/{id}")
+    @RequestMapping(value="/find/{id}",produces = "application/json;charset=utf-8")
     public String find(@PathVariable int id){
         try{
-            return userMapper.search(id).toString();
+            ObjectMapper om=new ObjectMapper();
+            return om.writeValueAsString(userMapper.search(id));
         }catch (Exception e){
             e.printStackTrace();
             return "no user!";
